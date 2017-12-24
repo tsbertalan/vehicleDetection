11b7a16d91d4488d7d4041704cf317a2322c9992
Tom Bertalan <tom@tombertalan.com>
2017-12-24 10:45:49 -0500

    Add lesson code.

---
+++ vehicleDetection/classify.py
@@ -0,0 +1,7 @@
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+# NOTE: the next import is only valid for scikit-learn version <= 0.17
+# for scikit-learn >= 0.18 use:
+# from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
+
